f7f0849614194e38832e1bfae8715162fa72fa19
Julian Barrera <ejbarrera0614@gmail.com>
2020-10-03 18:21:51 -0500

    Docs agregado

---
+++ docs/precache-manifest.e366c528994a037cc6032b4408f5b54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc07331b3c7a8a137e693bfdf23bff79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928c32446998ce1deb1a",
+    "url": "/static/css/main.fcde86fd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928c32446998ce1deb1a",
+    "url": "/static/js/main.f66237c6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
